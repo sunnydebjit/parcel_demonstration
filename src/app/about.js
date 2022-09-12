9ac5af9e796db7a00a3b58fee775fe01ba1af0c3
Debjit Majumdar <sunnydebjit@gmail.com>
2022-09-12 13:56:11 +0530

    HMR Initialised

--- src/app/about.js
+++ src/app/about.js
@@ -1,1 +1,5 @@
 console.log("This is the about page");
+
+if (module.hot) {
+  module.hot.accept();
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,9 @@
-console.log("Javascript Connected");
+console.log("This is the HomePage");
+
+// 1 Hot Module Reloading Script
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+console.log("Toggle this comment to check HMR");
